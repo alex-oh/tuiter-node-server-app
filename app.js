acdc5c8e7b4741ce716b3afad00bf5ffbc6017a4
alex-oh <oh.alexj@gmail.com>
2023-08-10 21:00:15 -0500

    refactor for public remote server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
+import "dotenv/config";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
@@ -9,7 +10,7 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: "http://localhost:3000",
+        origin: process.env.FRONTEND_URL,
     })
 );
 const sessionOptions = {
@@ -17,6 +18,13 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
 app.use(session(sessionOptions));
 app.use(express.json());
 const port = process.env.PORT || 4000;
@@ -25,4 +33,4 @@ TuitsController(app);
 HelloController(app);
 UserController(app);
 AuthController(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
