480998b5361367d8a758a5060cf98dcc63b12497
alex-oh <oh.alexj@gmail.com>
2023-08-14 16:57:54 -0400

    attempt fix of remote server connection

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
-import "dotenv/config";
+import 'dotenv/config';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
+
 const app = express();
 app.use(
     cors({
