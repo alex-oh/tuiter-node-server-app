b56fbe9b0665c66930f8f7d48ecf16031d314c17
alex-oh <oh.alexj@gmail.com>
2023-08-16 01:21:40 -0400

    updated mongoose connection

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ import UserController from "./controllers/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/tuiter";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 
 const app = express();
 // connect mongoose to the tuiter database
-// console.log(CONNECTION_STRING);
-mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
+console.log(CONNECTION_STRING);
+// mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
 mongoose.connect(CONNECTION_STRING);
 app.use(
     cors({
--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,30 +1,27 @@
-// import posts from "./tuits.js";
-// let tuits = posts;
 import * as tuitsDao from "../../tuits/tuits-dao.js";
 
-const createTuit = async (req, res) => {
-    const newTuit = req.body;
-    newTuit.likes = 0;
-    newTuit.liked = false;
-    // append new tuit to tuits array
-    const insertedTuit = await tuitsDao.createTuit(newTuit);
-    res.json(newTuit);
-};
 const findTuits = async (req, res) => {
     const tuits = await tuitsDao.findTuits(); // retrieve tuits from database
     res.json(tuits);
 };
-const updateTuit = async (req, res) => {
-    const tuitIdToUpdate = req.params["tid"];
-    const updates = req.body;
-    const status = await tuitsDao.updateTuit(tuitIdToUpdate, updates)
-    res.sendStatus(status);
+const createTuit = async (req, res) => {
+    const newTuit = req.body;
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
 };
 const deleteTuit = async (req, res) => {
     // retrieve the ID of the tuit we want to remove
     const tuitdIdToDelete = req.params["tid"];
     const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
     res.json(status);
+};
+const updateTuit = async (req, res) => {
+    const tuitIdToUpdate = req.params["tid"];
+    const updates = req.body;
+    const status = await tuitsDao.updateTuit(tuitIdToUpdate, updates)
+    res.sendStatus(status);
 };
 export default (app) => {
     app.post("/api/tuits", createTuit);
